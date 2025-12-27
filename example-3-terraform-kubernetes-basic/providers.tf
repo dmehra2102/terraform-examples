95c06f6a5bf19455887da7df2d27e4fa452b864f
dmehra2102 <deepanshumehra2102@gmail.com>
2025-12-27 11:00:22 +0000

    feat: added kubernetes provider with cred config

--- example-3-terraform-kubernetes-basic/providers.tf
+++ example-3-terraform-kubernetes-basic/providers.tf
@@ -1,3 +1,9 @@
 provider "aws" {
     region = "ap-south-1"
 }
+
+provider "kubernetes" {
+    host = module.eks_cluster.eks_cluster_api_server_endpoint
+    cluster_ca_certificate = base64decode(module.eks_cluster.eks_certificate_authority_data)
+    token = module.eks_cluster.eks_cluster_id
+}
