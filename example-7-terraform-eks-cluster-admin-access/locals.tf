b16a96fb7ed8b3d84f440947525c3d0b3de338f8
dmehra2102 <deepanshumehra2102@gmail.com>
2025-12-31 15:50:51 +0000

    feat: added locals variable

--- example-7-terraform-eks-cluster-admin-access/locals.tf
+++ example-7-terraform-eks-cluster-admin-access/locals.tf
@@ -1,3 +1,7 @@
 locals {
     eks_cluster_master_subnet_ids = data.terraform_remote_state.my_vpc.outputs.private_subnet_ids
 }
+
+locals {
+    iam_openid_connect_provider_url = aws_eks_cluster.my_eks_Cluster.identity[0].oidc[0].issuer
+}
