fd086eda2db779dbe72d14a43007d97e61d1aba5
dmehra2102 <deepanshumehra2102@gmail.com>
2026-01-01 02:52:09 +0000

    feat: added kubernetes and helm provider configuration

--- example-8-eks-golang-project/terraform/providers.tf
+++ example-8-eks-golang-project/terraform/providers.tf
@@ -27,15 +27,24 @@ provider "aws" {
     }
 }
 
+data "aws_eks_cluster_auth" "main" {
+    name = aws_eks_cluster.main.name
+}
+
 provider "kubernetes" {
+    host = aws_eks_cluster.main.endpoint
+    cluster_ca_certificate = base64decode(aws_eks_cluster.main.certificate_authority[0].data)
+    token = data.aws_eks_cluster_auth.main.token
 }
 
 provider "helm" {
+    kubernetes = {
+        host = aws_eks_cluster.main.endpoint
+        cluster_ca_certificate = base64decode(aws_eks_cluster.main.certificate_authority[0].data)
+        token = data.aws_eks_cluster_auth.main.token
+    }
 }
 
-data "aws_eks_cluster_auth" "main" {
-    name = ""
-}
 
 data "aws_caller_identity" "current" {
 }
