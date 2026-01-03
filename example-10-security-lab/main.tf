385ff0812eae1299571d90bc78af5f007cd89964
dmehra2102 <deepanshumehra2102@gmail.com>
2026-01-03 14:42:25 +0000

    feat: no access to junior-dev role

---
+++ example-10-security-lab/main.tf
@@ -0,0 +1,52 @@
+module "vpc" {
+    source  = "terraform-aws-modules/vpc/aws"
+    version = "~> 6.0"
+
+    name = "security-lab-vpc"
+    cidr = "10.0.0.0/16"
+
+    azs = ["ap-south-1a","ap-south-1b"]
+    private_subnets = ["10.0.1.0/24","10.0.2.0/24"]
+    public_subnets = ["10.0.101.0/24", "10.0.102.0/24"]
+    enable_nat_gateway = true
+
+    private_subnet_names = ["security-lab-private-subnet-1", "security-lab-private-subnet-2"]
+    public_subnet_names = ["security-lab-public-subnet-1", "security-lab-public-subnet-2"]
+}
+
+module "eks" {
+    source  = "terraform-aws-modules/eks/aws"
+    version = "~> 21.0"
+
+    name = "security-lab-cluster"
+    kubernetes_version = "1.34"
+    vpc_id = module.vpc.vpc_id
+    subnet_ids = module.vpc.private_subnets
+
+    endpoint_public_access = true
+
+    authentication_mode = "API_AND_CONFIG_MAP"
+
+    enable_cluster_creator_admin_permissions = true
+}
+
+resource "aws_iam_role" "junior_dev" {
+    name = "junior-dev-role"
+    assume_role_policy = jsonencode({
+        Version = "2012-10-17"
+        Statement = [
+            {
+                Action = "sts:AssumeRole"
+                Effect = "Allow"
+                Principal = {
+                    AWS = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
+                }
+            }
+        ]
+    })
+}
+
+data "aws_caller_identity" "current" {}
+
+output "cluster_name" { value = module.eks.cluster_name }
+output "junior_dev_role_arn" { value = aws_iam_role.junior_dev.arn }
