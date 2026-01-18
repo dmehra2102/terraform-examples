0e47c1227b669ffb70f19fd944f5087ede4e17bf
dmehra2102 <deepanshumehra2102@gmail.com>
2026-01-18 12:32:49 +0000

    feat: added Security Group for ECS task

--- terraform-ecs-fargate/security/main.tf
+++ terraform-ecs-fargate/security/main.tf
@@ -53,3 +53,44 @@ resource "aws_vpc_security_group_egress_rule" "alb_egress" {
         Name = "allow-all-egress"
     }
 }
+
+# ================================
+# ECS Task Security Group
+# ================================
+resource "aws_security_group" "ecs_task" {
+    name = "${var.project_name}-${var.environment}-ecs-task-sg"
+    description = "Security group for ECS tasks - allows traffic only from ALB"
+    vpc_id = var.vpc_id
+
+    tags = {
+        Name = "${var.project_name}-${var.environment}-ecs-task-sg"
+    }
+}
+
+# Allow traffic from ALB only on container port
+resource "aws_vpc_security_group_ingress_rule" "ecs_task_from_alb" {
+    security_group_id = aws_security_group.ecs_task.id
+    description = "Allow taffic from ALB on container port"
+
+    from_port = var.container_port
+    to_port = var.container_port
+    ip_protocol = "tcp"
+    referenced_security_group_id = aws_security_group.alb.id
+
+    tags = {
+        Name = "allow-alb-ingress"
+    }
+}
+
+# Allow all outbound traffic (for pulling images, accessing AWS services, external APIs)
+resource "aws_vpc_security_group_egress_rule" "ecs_task_egress" {
+    security_group_id = aws_security_group.ecs_task.id
+    description = "Allow all outbound traffic"
+
+    ip_protocol = "-1"
+    cidr_ipv4 = "0.0.0.0/0"
+
+    tags = {
+        Name = "allow-all-egress"
+    }
+}
