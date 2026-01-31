fba490e5cb223d5ca3cc2ec60090636c9dfc46e0
dmehra2102 <deepanshumehra2102@gmail.com>
2026-01-31 12:47:29 +0000

    feat: added terraform providers and versions

---
+++ kafka-eks-infra/terraform/versions.tf
@@ -0,0 +1,28 @@
+terraform {
+    required_version = ">=1.6.0"
+    required_providers {
+        aws = {
+            source = "hashicorp/aws"
+            version = "~> 6.0"
+        }
+        kubernetes = {
+            source = "hashicorp/kubernetes"
+            version = "~> 3.0"
+        }
+        helm = {
+            source  = "hashicorp/helm"
+            version = "~> 3.0"
+        }
+        tls = {
+            source  = "hashicorp/tls"
+            version = "~> 4.0"
+        }
+    }
+
+    backend "s3" {
+        bucket = "terraform-kafka-eks"
+        key = "terraform.tfstate"
+        encrypt = true
+        dynamodb_table = "terraform-state-lock"
+    }
+}
